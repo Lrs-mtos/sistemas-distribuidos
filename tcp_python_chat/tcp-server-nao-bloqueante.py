ac845209195f6ee1fcec5a07a0d7d6648a99572d
Larissa Matos <larissamatos07.lm@gmail.com>
2023-09-19 14:18:01 -0300

    edit on non blocking chat

--- tcp_python_chat/tcp-server-nao-bloqueante.py
+++ tcp_python_chat/tcp-server-nao-bloqueante.py
@@ -1,28 +1,51 @@
-from socket import *
-import threading
+import socket
+import select
 
-def receive_message(sock):
-    while True:
-        message = sock.recv(1024)
-        if not message:
-               break
-        print("\nFrom Client:", message.decode('utf-8'))
+# Configurações do servidor
+HOST = ''
+PORT = 12000
 
+# Criação do socket do servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(5)  # Define o número máximo de conexões em espera
 
-serverPort = 12001
-serverSocket = socket(AF_INET,SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
-print("The server is ready to receive")
+print(f"Servidor ouvindo em {HOST}:{PORT}")
 
-connectionSocket, addr = serverSocket.accept()
+# Lista de sockets para seleção (leitura)
+sockets_list = [server_socket]
 
-#Thread para receber mensagens do cliente
-receive_thread = threading.Thread(target=receive_message, args=(connectionSocket,))
-receive_thread.start()
+while True:
+    # Usa select para monitorar os sockets para leitura, escrita e erro
+    read_sockets, _, _ = select.select(sockets_list, [], [])
 
-while 1:
-     sentence = input('Input your message: ')
-     connectionSocket.send(sentence.encode('utf-8'))
+    for sock in read_sockets:
+        if sock == server_socket:
+            # Novo cliente se conectou
+            client_socket, client_address = server_socket.accept()
+            sockets_list.append(client_socket)
+            print(f"Cliente conectado: {client_address}")
 
-connectionSocket.close()
+            # Envia uma mensagem de boas-vindas
+            client_socket.send("Bem-vindo ao servidor!".encode())
+        else:
+            # Um cliente existente enviou uma mensagem
+            try:
+                message = sock.recv(1024).decode()
+                if message:
+                    print(f"Recebido: {message}")
+
+                    # Broadcast da mensagem para todos os clientes (exceto o remetente)
+                    for socket_item in sockets_list:
+                        if socket_item != server_socket and socket_item != sock:
+                            try:
+                                socket_item.send(message.encode())
+                            except:
+                                # Remove o socket se a conexão estiver quebrada
+                                socket_item.close()
+                                sockets_list.remove(socket_item)
+            except:
+                # Remove o socket se ocorrer um erro
+                sock.close()
+                sockets_list.remove(sock)
+                continue
